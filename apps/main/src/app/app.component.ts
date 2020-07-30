4f983b56884d4bc716bea080832a197dda05ecf8
Crowhurst, Christian <Christian.Crowhurst@mrisoftware.com>
2020-07-30 21:12:36 +0100

    initial commit

---
+++ apps/main/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rx-state-playground-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'main';
+}
